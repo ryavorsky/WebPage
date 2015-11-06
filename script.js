b44b257a262bfdd8d9560f9ac2c3861e018228a4
ryavorsky <ryavorsky@gmail.com>
2015-11-06 14:09:01 +0300

    Upd

--- script.js
+++ script.js
@@ -33,49 +33,26 @@ function search(event){
 var directionsDisplay;
 var directionsService = new google.maps.DirectionsService();
 var geocoder = new google.maps.Geocoder();
+
+var map_center;
+var places;
+
 var map1;
 var map2;
 var map3;
-var map4;
-var maps = [map1, map2, map3, map4]
-var goldStar = {
-    path: 'M 125,5 155,90 245,90 175,145 200,230 125,180 50,230 75,145 5,90 95,90 z',
-    fillColor: 'rgba(255,255,55,0.75)',
-    fillOpacity: 0.8,
-    scale: 0.07,
-    strokeColor: 'rgba(100,100,100,0.5)',
-    strokeWeight: 2
-  };
+var maps = [];
 
 var marker1 = new google.maps.Marker({map: map1});
 var marker2 = new google.maps.Marker({map: map2});
-var marker3 = new google.maps.Marker({icon: goldStar, map: map3});
-var marker4 = new google.maps.Marker({map: map4});
-var markers = [marker1, marker2, marker3, marker4]
-var image1 = 'red_flag32.png'
-var image2 = 'red_flag.png'
-var image3 = 'red_ball.png'
-var pinIcon1 = new google.maps.MarkerImage(
-    "http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|FFEE10",
-    null, /* size is determined at runtime */
-    null, /* origin is 0,0 */
-    null, /* anchor is bottom center of the scaled image */
-    new google.maps.Size(10, 15)
-); 
-var pinIcon2 = new google.maps.MarkerImage(
-    "http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|DDEE10",
-    null, /* size is determined at runtime */
-    null, /* origin is 0,0 */
-    null, /* anchor is bottom center of the scaled image */
-    new google.maps.Size(13, 21)
-); 
-var pinIcon3 = new google.maps.MarkerImage(
-    "http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|FF8888"
-); 
+var marker3 = new google.maps.Marker({map: map3});
+var markers = [marker1, marker2, marker3];
+var image1 = 'red_flag32.png';
+var image2 = 'red_flag.png';
+var image3 = 'red_ball.png';
+var images = [image1, image2, image3];
 
 function initialize() {
-  //directionsDisplay = new google.maps.DirectionsRenderer();
-  var mapOptions1 = {
+   var mapOptions1 = {
     zoom:3,
     disableDefaultUI: true
   };
@@ -85,53 +62,76 @@ function initialize() {
   };
   var mapOptions3 = {
     zoom:16,
-    disableDefaultUI: true,
-    styles: [{"featureType":"road.highway","elementType":"labels","stylers":[{"hue":"#ffffff"},{"saturation":-100},{"lightness":100},{"visibility":"off"}]},{"featureType":"landscape.natural","elementType":"all","stylers":[{"hue":"#ffffff"},{"saturation":-100},{"lightness":100},{"visibility":"on"}]},{"featureType":"road","elementType":"all","stylers":[{"hue":"#ffe94f"},{"saturation":100},{"lightness":4},{"visibility":"on"}]},{"featureType":"road.highway","elementType":"geometry","stylers":[{"hue":"#ffe94f"},{"saturation":100},{"lightness":4},{"visibility":"on"}]},{"featureType":"water","elementType":"geometry","stylers":[{"hue":"#333333"},{"saturation":-100},{"lightness":-74},{"visibility":"off"}]}]
-  };
-  var mapOptions4 = {
-    zoom:16,
-    disableDefaultUI: true,
-    mapTypeId: google.maps.MapTypeId.HYBRID
+    disableDefaultUI: true
   };
   map1 = new google.maps.Map(document.getElementById('google_map1'), mapOptions1);
   map2 = new google.maps.Map(document.getElementById('google_map2'), mapOptions2);
   map3 = new google.maps.Map(document.getElementById('google_map3'), mapOptions3);
-  map4 = new google.maps.Map(document.getElementById('google_map4'), mapOptions4);
-  maps = [map1, map2, map3, map4];
-  //directionsDisplay.setMap(map1);
-  cmap(1);
-  cmap(2);
-  cmap(3);
-  cmap(4);
-}
+  maps = [map1, map2, map3];
 
-function cmap(i){
-  markers[i-1].setMap(null);
-  pa = document.getElementById("post_addr_value");
-  var address = pa.innerHTML;
+  // Use geocoding to get the center coordinates
+  post_addr = document.getElementById("post_addr_value");
+  var address = post_addr.innerHTML;
   geocoder.geocode({'address': address}, function(results, status) {
-    if (status === google.maps.GeocoderStatus.OK) {
-      maps[i-1].setCenter(results[0].geometry.location);
-      if(i<4){
-          marker = new google.maps.Marker({
-            map: maps[i-1],
-            position: results[0].geometry.location
-          });
-      }
-      if (i==1){
-        marker.setIcon(image1);
-      };
-      if (i==2){
-        marker.setIcon(image2);
-      }
-      if (i==3){
-        marker.setIcon(image3);
-      };
+    if (status === google.maps.GeocoderStatus.OK) { 
+        map_center = results[0].geometry.location;
+        for(var i=0; i<3; i++){
+            maps[i].setCenter(map_center);
+            marker = new google.maps.Marker({
+                map: maps[i],
+                position: map_center
+            });
+            marker.setIcon(images[i]);
+        };
+        get_places(1000);
     } else {
       alert('Geocode was not successful for the following reason: ' + status);
     }
   });
+  
+
 }
+
+
+function get_places(range) {
+    var request = {
+        radius: range.toString(),
+        //rankBy: google.maps.places.RankBy.DISTANCE,
+        location: map_center
+    };
+    var service = new google.maps.places.PlacesService(map3);
+    service.nearbySearch(request, places_callback);
+}
+
+function places_callback(results , status, pagination) {
+    if (status == google.maps.places.PlacesServiceStatus.OK) {
+            places = results;
+    };
+    
+    icons=[];
+    res = [];
+    space = '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;';
+    for (i=0; i < places.length; i++){
+        big_img = '<img width="22px" src="' + places[i].icon +'" / > ';
+        small_img = '<img width="16px" src="' + places[i].icon +'" / > ';
+        name = places[i].name + ', ';
+        addr = '<a href="">' + places[i].vicinity + '</a>';
+        
+        icons.push(big_img);
+        res.push(small_img + name + addr);
+    };
+    
+    icons = icons.sort();
+    for (i=1; i<icons.length; i++){
+        if (icons[i] != icons[i-1]){
+            icons[i-1] = icons[i-1] + space;
+        }
+    }
+    e = document.getElementById('places');
+    e.innerHTML = e.innerHTML + space + icons.join('') + '<p>' + res.join('; ' + space);
+
+}
+
 
 function calcRoute() {
 
